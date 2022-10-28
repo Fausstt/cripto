3fd802303f27d1aaa7fb66642e772c1b5c6298f7
Nastia-rosy <nastiiarosy@gmail.com>
2022-10-28 08:00:59 +0300

    crypto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material";
+import "./styles/root.css";
+import "./styles/Animation.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./components/App";
+
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 850,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+    palette: {
+        primary: {
+            main: "#000",
+        },
+        secondary: {
+            light: "#1B95E0",
+            main: "#1B95E0",
+            dark: " #0057B7",
+            contrastText: "#fff",
+        },
+        secondary02: {
+            light: "#5DC948",
+            main: "#5DC948",
+            dark: " #5DC948",
+            contrastText: "#5DC948",
+        },
+    },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
